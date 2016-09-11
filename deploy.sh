f0f68454e05d0d5915e16dfc0e136a0cabcc6a2d
AMiT Kumar <dtu.amit@gmail.com>
2016-09-11 18:18:27 +0300

    fix directory in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
-cp blog-gen/_site/timeline/index.html aktech.github.io/timeline/
-cp blog-gen/_site/tags/index.html aktech.github.io/tags
-cp -rf blog-gen/_site/feeds/ aktech.github.io/blog/feeds/
-cp -rf blog-gen/_site/assets/ aktech.github.io/assets/
-cp blog-gen/_site/index.html aktech.github.io/blog/
-cp blog-gen/_site/talks/index.html aktech.github.io/talks
+cp blog-gen/_site/timeline/index.html timeline/
+cp blog-gen/_site/tags/index.html tags
+cp -rf blog-gen/_site/feeds/ blog/feeds/
+cp -rf blog-gen/_site/assets/ assets/
+cp blog-gen/_site/index.html blog/
+cp blog-gen/_site/talks/index.html talks
