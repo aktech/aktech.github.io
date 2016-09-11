6930d2819fd08f17ab9a35d35baaeb25a8f80821
AMiT Kumar <dtu.amit@gmail.com>
2016-09-11 18:13:56 +0300

    add blog generator

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+cp blog-gen/_site/timeline/index.html aktech.github.io/timeline/
+cp blog-gen/_site/tags/index.html aktech.github.io/tags
+cp -rf blog-gen/_site/feeds/ aktech.github.io/blog/feeds/
+cp -rf blog-gen/_site/assets/ aktech.github.io/assets/
+cp blog-gen/_site/index.html aktech.github.io/blog/
+cp blog-gen/_site/talks/index.html aktech.github.io/talks
