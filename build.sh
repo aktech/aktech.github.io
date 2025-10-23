6a7e7ca6b7a5304324bb9f6a403866fe959b34a8
Amit Kumar <dtu.amit@gmail.com>
2025-10-23 02:56:29 +0100

    fix git commit hash

--- build.sh
+++ build.sh
@@ -3,7 +3,18 @@
 # Build script for Hugo site
 set -e
 
-echo "Building Hugo site..."
+# Get git commit hash and create data file
+mkdir -p data
+GIT_COMMIT_HASH=$(git rev-parse --short HEAD)
+GIT_COMMIT_HASH_FULL=$(git rev-parse HEAD)
+cat > data/git.json <<EOF
+{
+  "hash": "$GIT_COMMIT_HASH",
+  "hashFull": "$GIT_COMMIT_HASH_FULL"
+}
+EOF
+
+echo "Building Hugo site (commit: $GIT_COMMIT_HASH)..."
 hugo --gc --minify
 
 echo "Creating atom.xml from index.xml..."
